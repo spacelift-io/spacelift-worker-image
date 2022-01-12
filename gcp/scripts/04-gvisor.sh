18354fbb30f28d99916dc8dd6e8eca855a83a0b3
Michał Goliński <3693729+michalg9@users.noreply.github.com>
2022-01-12 11:30:45 +0100

    Update gvisor installation to fetch newest version (#21)

--- gcp/scripts/04-gvisor.sh
+++ gcp/scripts/04-gvisor.sh
@@ -1,11 +1,15 @@
 # This script installs the gVisor.
 set -e
 
-URL=https://storage.googleapis.com/gvisor/releases/release/latest
+ARCH=$(uname -m)
+URL=https://storage.googleapis.com/gvisor/releases/release/latest/${ARCH}
 
-wget ${URL}/runsc -P /tmp
-sudo mv /tmp/runsc /usr/local/bin
-sudo chmod a+rx /usr/local/bin/runsc
+wget ${URL}/runsc ${URL}/runsc.sha512 ${URL}/containerd-shim-runsc-v1 ${URL}/containerd-shim-runsc-v1.sha512
+sha512sum -c runsc.sha512 -c containerd-shim-runsc-v1.sha512
+rm -f *.sha512
+
+chmod a+rx runsc containerd-shim-runsc-v1
+sudo mv runsc containerd-shim-runsc-v1 /usr/local/bin
 
 sudo /usr/local/bin/runsc install -- --fsgofer-host-uds
 sudo systemctl restart docker
