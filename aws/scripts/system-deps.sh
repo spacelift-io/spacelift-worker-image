a51beada619277f3cd95caffcddea8104c66b768
peterdeme <snypox@gmail.com>
2024-07-02 21:03:38 +0200

    Introduce `dnf upgrade`

--- aws/scripts/system-deps.sh
+++ aws/scripts/system-deps.sh
@@ -1,3 +1,6 @@
+# Deterministic upgrades through versioned repositories usage
+sudo dnf upgrade -y --releasever="latest"
+
 # We need it for binary verification.
 sudo dnf swap -y gnupg2-minimal gnupg2-full
 
