20092bf19ed9ac1683dcfcce7a526a667bd304f7
Marcin Prokop <marcin@prokop.co>
2026-05-06 17:53:35 +0200

    Mitigate CVE-2026-31431 (Copy Fail) by upgrading kmod on Azure

---
+++ azure/scripts/kmod-upgrade.sh
@@ -0,0 +1,5 @@
+# Mitigates CVE-2026-31431 (Copy Fail) - upgrades kmod to a version that blocks
+# the algif_aead kernel module, which enables local privilege escalation to root.
+# More: https://ubuntu.com/blog/copy-fail-vulnerability-fixes-available
+
+sudo apt update && sudo apt install --only-upgrade kmod
