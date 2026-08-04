7ad6c700a24d53ecb9a21fa5b574969c29193273
Wojciech Przechrzta <wojciechp@spacelift.io>
2026-08-04 13:45:31 +0200

    feat: Adds test stack

--- infra/stacks/workerpool-aws-gov/outputs.tf
+++ infra/stacks/workerpool-aws-gov/outputs.tf
@@ -6,6 +6,6 @@ output "asg_name" {
   value = module.workerpool.autoscaling_group_name
 }
 
-# The ASG-refresh (cycle) role is a manual prerequisite in 259242304461:
-#   arn:aws-us-gov:iam::259242304461:role/deployment-spacelift-worker-image
-# Set that ARN as the repo secret consumed by the CI cycle step.
+output "test_stack_id" {
+  value = spacelift_stack.test.id
+}
