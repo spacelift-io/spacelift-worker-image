aaacdb4b553eaa84ce3390eacb5843bd6f929e39
Wojciech Przechrzta <wojciechp@spacelift.io>
2026-08-10 11:28:25 +0200

    feat: Run checks

--- infra/stacks/workerpool-aws/variables.tf
+++ infra/stacks/workerpool-aws/variables.tf
@@ -1,4 +1,4 @@
 variable "ami_id" {
   type        = string
-  description = "AMI to launch workers from. Injected at run time via TF_VAR_ami_id; never committed."
+  description = "AMI to launch workers from. Injected at run time via TF_VAR_ami_id."
 }
